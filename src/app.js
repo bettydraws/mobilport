a1bae4f2eba1dfc285a56c7d45b6be686b93eb0f
bettydraws <imedia@imd-im-54a.camp.confederationc.on.ca>
2018-03-20 14:41:52 -0400

    Added the button to count

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-const {Composite, TextView, ui} = require('tabris');
+const {Button, Composite, TextView, ui} = require('tabris');
 
 // Create composites and append children to them
+// Create a push button that counts up on selection
+
 
 let composite1 = new Composite({
   left: 0, top: 0, bottom: 0, right: '50%',
   background: '#f3f3f3'
 }).appendTo(ui.contentView);
+
+let count = 0;
+
+new Button({
+  left: 10, top: 10,
+  text: 'Button'
+}).on('select', ({target}) => target.text = 'Pressed ' + (++count) + ' times')
+  .appendTo(ui.contentView);
 
 new TextView({
   left: 0, right: 0, top: '50%', 
